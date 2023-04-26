0e83220ba6ec432552207d1f11a212f2a00ba709
c3z <cezdmo@gmail.com>
2023-04-26 20:43:40 +0200

    style(nvim): format lua files with linter feat(nvim): add pyright language server to mason-lspconfig's ensure_installed list feat(nvim): add keymaps for LSP hover, definition, and implementation refactor(nvim): rename lsp.lua to c3z.lsp.lua

---
+++ nvim/nvim.conf-symlink/after/plugin/lsp.lua
@@ -0,0 +1,6 @@
+local nnoremap = require("c3z.keymap").nnoremap
+
+nnoremap("K", vim.lsp.buf.hover)
+nnoremap("<leader>gd", vim.lsp.buf.definition)
+nnoremap("<leader>gi", vim.lsp.buf.implementation)
+
