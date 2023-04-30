64b6b5116262650f02915fbb74e400022f93a64b
c3z <cezdmo@gmail.com>
2023-04-30 11:34:41 +0200

    feat(set_config.sh): add script to set poetry virtualenvs.in-project to true feat(install.sh): add function to clone tmux tpm plugin if it does not exist, and call it in the script. Also, install tmux using brew.

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,6 +1,15 @@
-if test ! "$(uname)" = "Darwin"
-  then
-  exit 0
-fi
+function clone_if_not_exists() {
+  local repo_url="https://github.com/tmux-plugins/tpm"
+  local target_dir="${HOME}/.tmux/plugins/tpm"
+
+  if [[ ! -d "${target_dir}" ]]; then
+    echo "Tmux tpm plugin does not exist, cloning repository..."
+    git clone "${repo_url}" "${target_dir}"
+  else
+    echo "Plugin already exists, skipping."
+  fi
+}
 
 brew install "tmux"
+clone_if_not_exists
+
