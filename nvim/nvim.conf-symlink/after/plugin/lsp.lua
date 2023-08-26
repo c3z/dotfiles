ac09f4b352a7f54f0d0083c2170c90f19cad435d
c3z <cezdmo@gmail.com>
2023-08-26 21:25:34 +0200

    chore(lsp.lua): update Lua LSP configuration to include "use" in diagnostics globals chore(lsp.lua): add TypeScript LSP configuration

--- nvim/nvim.conf-symlink/after/plugin/lsp.lua
+++ nvim/nvim.conf-symlink/after/plugin/lsp.lua
@@ -1,9 +1,8 @@
-
 require("mason").setup()
 require("mason-lspconfig").setup({
     ensure_installed = { "lua_ls", "pyright" },
 })
 
-require("lspconfig").lua_ls.setup({settings={ Lua = { diagnostics = { globals = { "vim" } }} } })
+require("lspconfig").lua_ls.setup({ settings = { Lua = { diagnostics = { globals = { "use", "vim" } } } } })
 require("lspconfig").pyright.setup({ filetypes = { "python", "py" } })
 require("lspconfig").tsserver.setup({})
