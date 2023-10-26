f765be705e5abdfdce14d1259c147705a7f77e59
c3z <cezdmo@gmail.com>
2023-10-26 20:29:54 +0200

    feat(git/aliases.zsh): add 'oc' alias for 'opencommit' command to improve productivity and convenience

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,4 +20,5 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
 alias lg='lazygit'
+alias oc='opencommit'
 
