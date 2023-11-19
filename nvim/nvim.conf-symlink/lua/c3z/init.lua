bd4433df1180489734bb3925d42c06820121511a
c3z <cdm@itsg.com.pl>
2023-11-19 15:18:44 +0100

    New key mappings (#7)
    * (wip) cleaning keymaps to confront one status
    
    * Removed unecessary file
    
    * wip

--- nvim/nvim.conf-symlink/lua/c3z/init.lua
+++ nvim/nvim.conf-symlink/lua/c3z/init.lua
@@ -1,10 +1,7 @@
 require("c3z.set")
 require("c3z.packer")
 require("c3z.remap")
-require("c3z.keymap")
 require("c3z.ngit")
-require("c3z.telescope")
 require("c3z.tmuxnavigator")
 require("c3z.lsp")
 require("c3z.python")
-
--- nvim/nvim.conf-symlink/lua/c3z/remap.lua
+++ nvim/nvim.conf-symlink/lua/c3z/remap.lua
@@ -1,15 +1,49 @@
- local M = require("c3z.util-map")
-  local opts = { noremap = true, silent = true, }
- --wk.register({
-    --[""] = { "", "" },
-  --  ["K"] = { "vim.lsp.buf.hover", "Hover to buf" },
-   -- ["<leader>g"] = { name = "Go to" },
-   -- ["<leader>gd"] = { "<cmd>lua vim.lsp.buf.definition<CR>", "Go to definition" },
-   -- ["<leader>gi"] = { ":lua vim.lsp.buf.implementation<CR>", "Go to implementation" },
-   -- ["<leader>pv"] = { "<cmd>Ex<CR>", "Explorer" },
-   -- ["<C-o>"] = { ':lua require("harpoon.ui").toggle_quick_menu()<CR>', "Toggle harpoon mark" },
-   -- ["<C-i>"] = { ':lua require("harpoon.mark").add_file()<CR>', "Add file to harpoon mark" },
-   -- ["<C-w>"] = { "w<CR>", "Save file" },
+local M = require("c3z.util-map")
+local opts = { noremap = true, silent = true }
+--wk.register({
+--[""] = { "", "" },
+--  ["K"] = { "vim.lsp.buf.hover", "Hover to buf" },
+-- ["<leader>g"] = { name = "Go to" },
+-- ["<leader>gd"] = { "<cmd>lua vim.lsp.buf.definition<CR>", "Go to definition" },
+-- ["<leader>gi"] = { ":lua vim.lsp.buf.implementation<CR>", "Go to implementation" },
+-- ["<leader>pv"] = { "<cmd>Ex<CR>", "Explorer" },
 --}, { mode = "n" })
 M.map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts, "LSP", "ngs-cmd-lua-vim-lsp-buf-hover", "Hover to buf")
-M.map("n", "<C-o>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", opts, "LSP", "ngs-cmd-lua-require--lspsaga-preview-definiotion", "LSP Saga Preview Definition")
+M.map("n", "<C-i>", "<cmd>lua require('harpoon.mark').add_file()<CR>", opts, "LSP", "ngs-cmd-lua-", "Harpoon mark")
+M.map("n", "<leader>cr", "<cmd>luafile %<CR>", opts, "CONFIG", "Reload current file", "Reload config")
+M.map(
+    "n",
+    "<leader>ps",
+    "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep >')})<CR>",
+    opts,
+    "Telescope",
+    "TelescopeGrepFind",
+    "Telescope grep find text"
+)
+M.map(
+    "n",
+    "<C-p>",
+    "<cmd>lua require('telescope.builtin').git_files()<CR>",
+    opts,
+    "Telescope",
+    "TelescopeGitFiles",
+    "Telescope git files"
+)
+M.map(
+    "n",
+    "<leader>pf",
+    "<cmd>lua require('telescope.builtin').find_files()<CR>",
+    opts,
+    "Telescope",
+    "TelescopeFind",
+    "Telescope find files"
+)
+M.map(
+    "n",
+    "<C-o>",
+    "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>",
+    opts,
+    "LSP",
+    "ngs-cmd-lua-require--lspsaga-preview-definiotion",
+    "LSP Saga Preview Definition"
+)
