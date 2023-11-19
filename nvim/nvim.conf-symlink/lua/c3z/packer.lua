36bf8814b078436fbb93f3bc5e3909d23117313c
c3z <cezdmo@gmail.com>
2023-11-19 17:56:45 +0100

    more plugins

--- nvim/nvim.conf-symlink/lua/c3z/packer.lua
+++ nvim/nvim.conf-symlink/lua/c3z/packer.lua
@@ -18,26 +18,12 @@ packer.startup(function()
         end,
     })
 
+    "github/copilot.vim")
 
-    use("christoomey/vim-tmux-navigator")
-    use("daschw/leaf.nvim")
-    use("vim-test/vim-test")
     use({
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
     })
 
-    use("neovim/nvim-lspconfig")
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("hrsh7th/cmp-cmdline")
-    use("hrsh7th/nvim-cmp")
-    use("hrsh7th/cmp-vsnip")
-    use("hrsh7th/vim-vsnip")
-
-    use("sbdchd/neoformat")
-    use("elentok/format-on-save.nvim")
-    use("github/copilot.vim")
 end)
--- nvim/nvim.conf-symlink/lua/c3z/plugins/init.lua
+++ nvim/nvim.conf-symlink/lua/c3z/plugins/init.lua
@@ -7,8 +7,25 @@ return {
     "christoomey/vim-tmux-navigator", -- tmux & split window navigation
     "ThePrimeagen/vim-be-good",
 
+    -- themes
     "folke/tokyonight.nvim",
     { "catppuccin/nvim", as = "catppuccin" },
+
+    -- other plugins
+    "christoomey/vim-tmux-navigator",
+    "daschw/leaf.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
+    "sbdchd/neoformat",
+    "elentok/format-on-save.nvim",
 }
 
+
+-- plugins to test
 --  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
